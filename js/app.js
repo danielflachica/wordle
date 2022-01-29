3fa957447c3ca03a4dce5c89a7cf263f2f9d70ac
Daniel Lachica <daniel.lachica82@gmail.com>
2022-01-29 15:28:26 +0800

    Bind keypresss to grid

--- js/app.js
+++ js/app.js
@@ -8,22 +8,32 @@ const ALPHABET = Array.from(Array(26)).map((e, i) => i + 97).map((x) => String.f
 const KEYPAD_KEYS = ALPHABET.concat(['enter', 'backspace']);
 
 var keypadButtons = document.getElementsByClassName("kp-btn");
+var gridCellContainers = document.getElementsByClassName("grid-cell");
 var guess = '';
 var guessAttempts = [];
 
 let keypad = new Keypad(keypadButtons, KEYPAD_KEYS);
+let board = new Board(MAX_LETTERS, MAX_GUESSES, gridCellContainers);
 
 /* ---------------------------------------------------------------- */
 
 // methods here
+let render = (key) => {
+    keypad.press(key);
+    board.addToGrid(key);
+    board.display();
+}
 
 /* ---------------------------------------------------------------- */
+
+// initialize grid object
+board.initGrid();
 
 // bind mouse clicks to keypad buttons
 for (var i = 0; i < keypad.buttons.length; i++) {
     keypad.buttons[i].addEventListener("click", function () {
         var key = this.value.toLowerCase();
-        keypad.press(key);
+        render(key);
     });
 }
 
@@ -31,6 +41,6 @@ for (var i = 0; i < keypad.buttons.length; i++) {
 document.addEventListener("keydown", function (event) {
     var key = event.key.toLowerCase();
     if(keypad.keys.includes(key)) {
-        keypad.press(key);
+        render(key);
     }
 });
--- js/board.js
+++ js/board.js
@@ -1,9 +1,62 @@
 // board.js
 
 class Board {
-    constructor(height, width) {
-        this.height = height;
+    constructor(width, height, gridCellContainers) {
         this.width = width;
+        this.height = height;
+        this.gridCellContainers = gridCellContainers;
         this.grid = [];
+        this.activeCell = {'row': 0, 'col': 0};
+    }
+
+    // init this.height x this.width grid with cell IDs as initial values
+    initGrid() {
+        for (var i = 0; i < this.height; i++) {
+            var row = [];
+            for (var j = 0; j < this.width; j++) {
+                row.push(null);
+            }
+            this.grid.push(row);
+        }
+    }
+
+    display() {
+        // display grid only if within bounds
+        if (this.activeCell.col < this.width && this.activeCell.row < this.height) {
+            console.log(this.grid);
+        }
+    }
+
+    addToGrid(letter) {
+        // add letter to internal grid and render if within bounds
+        if (this.activeCell.col < this.width && this.activeCell.row < this.height) {
+            this.grid[this.activeCell.row][this.activeCell.col] = letter;
+            this.render(letter);
+            this.incrementActiveCell();
+        }
+    }
+
+    render(letter) {
+        var cellArray = Array.from(this.gridCellContainers);
+        var activeCellID = this.activeCell.row.toString().concat(this.activeCell.col.toString());
+
+        for (var i = 0; i < cellArray.length; i++) {
+            if (cellArray[i].id == activeCellID) {
+                document.getElementById(activeCellID).innerHTML = letter;
+                break;
+            }
+        }
+    }
+
+    incrementActiveCell() {
+        // increment column if word not complete
+        if ((this.activeCell.col + 1) % this.width != 0) {
+            this.activeCell.col++;
+        }
+        // increment row and reset column if word complete
+        else {
+            this.activeCell.row++;
+            this.activeCell.col = 0;
+        }
     }
 }
